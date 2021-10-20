4467ff6394945d4bc5a9bb9459d9e667ead67a8b
vigneshiva <53216559+vigneshiva@users.noreply.github.com>
2021-10-20 12:12:43 +0530

    Add files via upload

---
+++ Vignesh_assignment.py
@@ -0,0 +1,56 @@
+from os import listdir
+from wand.image import Image
+from pptx import Presentation
+from pptx.util import Inches
+
+image_destination = input("Enter the image destination: ") + "\\"
+watermark_name = input("Enter the watermark's name:")
+watermark_file = image_destination + watermark_name + ".png"
+imagesList = listdir(image_destination)
+path = input("Enter the output destination: ") + "\\"
+
+
+def get_all_images(image_path, watermark, output_path):
+    imagesList = listdir(image_path)
+
+    for image in imagesList:
+        img = (image_path + image)
+        image_check = str(img).split(".")
+        if image_check[1] == 'jpg':
+            with Image(filename=img) as pic:
+                with Image(filename=watermark) as wm:
+                    wm.transparentize(0.11)
+                    with Image(width=pic.width, height=pic.height) as final:
+                        final.composite(image=pic, left=0, top=0)
+                        final.composite(image=wm, left=1, top=1)
+                        name = image.replace(".jpg", "")
+                        final.save(filename=output_path + name + ".png")
+
+
+def presentation(images_path):
+    prs = Presentation()
+
+    lyt = prs.slide_layouts[1]  # choosing a slide layout
+
+    # images from the path
+    imagesList = listdir(images_path)
+
+    for image in imagesList:
+        slide = prs.slides.add_slide(lyt)  # adding a slide
+        prs.slide_width = Inches(56)
+        prs.slide_height = Inches(56)
+        title = slide.shapes.title  # assigning a title
+        subtitle = slide.placeholders[1]  # placeholder for subtitle
+        name = image.replace(".jpg.png", "")
+        title.text = f"{name}"  # title
+        subtitle.text = f"{name}"  # subtitle
+
+        img = (images_path + image)
+
+        slide.shapes.add_picture(img, left=Inches(1), top=Inches(2.5))
+
+    prs.save(f'{images_path}\output.pptx')  # saving file
+
+
+get_all_images(image_destination, watermark_file, path)
+presentation(path)
